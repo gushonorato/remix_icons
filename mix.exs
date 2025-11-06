4a67021889f5da45c0a633430ba4537ecf5d716b
Gustavo Honorato <gustavohonorato@gmail.com>
2025-11-06 15:41:58 -0300

    fix icons not beign found when added as lib

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule RemixIcons.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -27,5 +28,23 @@ defmodule RemixIcons.MixProject do
       {:phoenix_live_view, ">= 0.0.0"},
       {:cachex, ">= 0.0.0"}
     ]
+  end
+
+  defp aliases do
+    [
+      remix_clone: &remix_clone/1,
+      remix_clean: &remix_clean/1,
+      compile: ["remix_clone", "compile"]
+    ]
+  end
+
+  defp remix_clone(_) do
+    if not File.exists?("priv/remix") do
+      System.cmd("git", ["clone", "git@github.com:Remix-Design/RemixIcon.git", "priv/remix"])
+    end
+  end
+
+  defp remix_clean(_) do
+    System.cmd("rm", ["-rf", "priv/remix"])
   end
 end
